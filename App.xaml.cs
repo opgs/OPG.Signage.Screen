2ffd6762a906c61f89d2eca330f1924c0816cbb6
Luke Bridges <LukeBridges@users.noreply.github.com>
2018-02-13 08:46:57 +0000

    Synced to latest

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,35 +1,38 @@
 ﻿using System.Diagnostics;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.LockScreen;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace OPG.Signage.Screen
 {
-    public sealed partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+	public sealed partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
 
-            UnhandledException += OnUnhandledException;
-        }
+			UnhandledException += OnUnhandledException;
+		}
 
-        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs exception)
-        {
-            Debug.WriteLine(exception.Message);
-            exception.Handled = true;
-        }
+		private void OnUnhandledException(object sender, UnhandledExceptionEventArgs exception)
+		{
+			Debug.WriteLine(exception.Message);
+			Debug.WriteLine(exception.Exception.StackTrace);
+			exception.Handled = true;
+		}
 
-        protected override void OnLaunched(LaunchActivatedEventArgs args)
-        {
-			if(args != null)
+		protected override void OnLaunched(LaunchActivatedEventArgs args)
+		{
+			if (args != null)
 			{
+				LockApplicationHost host = LockApplicationHost.GetForCurrentView();
 				Frame rootFrame = new Frame();
 				Window.Current.Content = rootFrame;
 				rootFrame.Navigate(typeof(MainPage), args.Arguments);
 				Window.Current.Activate();
 			}
-        }
-    }
+		}
+	}
 }
 
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using OPG.Signage.APIServer;
+using OPG.Reception.Printing;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.UI.Xaml;
@@ -7,67 +10,111 @@ using Windows.UI.Xaml.Controls;
 
 namespace OPG.Signage.Screen
 {
-    public sealed partial class MainPage : Page
+	public sealed partial class MainPage : Page
 	{
-		private string url = "http://signage.opgs.org";
-		private string cUrl = "http://signage.opgs.org";
-        private readonly ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+		private string url = "http://dev.opgs.org/signage/screen/";
+		private string cUrl = "http://dev.opgs.org/signage/screen/";
+		private readonly ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+		private PrintJob print;
+		private static int APIVersion = 5;
+		private string APIDescription = (new StreamReader(@"APIDescriptor.html")).ReadToEnd();		
+		private string API4replyformat = "{{\"os\":\"{0}\", \"wifi\":\"{1}\", \"ssid\":\"{2}\", \"url\":\"{3}\"}}";
+		private string API4Format = "{{\"apiversion\":\"" + APIVersion + "\",\"name\":\"" + Info.Network.HostName + "\",\"version\":\"" + Info.APP.AppVersion + "\",\"reply\":{0}}}";
 
-        private void RefreshTick()
-        {
-            if (cUrl != url)
-            {
-                if (cUrl == "refresh")
-                {
-                    ScreenView.Refresh();
-                    lock (cUrl) cUrl = url;
-                }
-                else
-                {
-                    ScreenView.Navigate(new Uri(cUrl));
-                    localSettings.Values["url"] = cUrl;
-                    url = cUrl;
-                }
-            }
-        }
+		private void RefreshTick()
+		{
+			if (cUrl != url)
+			{
+				if (cUrl == "refresh")
+				{
+					ScreenView.Refresh();
+					lock (cUrl) cUrl = url;
+				}
+				else
+				{
+					ScreenView.Navigate(new Uri(cUrl));
+					localSettings.Values["url"] = cUrl;
+					url = cUrl;
+				}
+			}
+		}
 
-        private void StartRefreshTimer()
-        {
-            DispatcherTimer refreshTimer = new DispatcherTimer
-            {
-                Interval = new TimeSpan(0, 0, 30)
-            };
-            refreshTimer.Tick += (_, __) => RefreshTick();
-            refreshTimer.Start();
-        }
+		private void StartRefreshTimer()
+		{
+			DispatcherTimer refreshTimer = new DispatcherTimer
+			{
+				Interval = new TimeSpan(0, 0, 30)
+			};
+			refreshTimer.Tick += (_, __) => RefreshTick();
+			refreshTimer.Start();
+		}
 
-        private void SetupServer()
-        {
-            JSONServer webserver = new JSONServer(8081, Info.Network.IP)
-            {
-                RegUrl = new Uri("https://dev.opgs.org/signage/index.php"),
-                ReplyFormat = "{{\"Name\":\"" + Info.Network.HostName + "\",\"Version\":\"" + Info.APP.AppVersion + "\",\"Reply\":\"{0}\"}}"
-            };
-            webserver.Get("/api/os", (_) => new JSONReply(Info.OS.Version));
-            webserver.Get("/api/wifi", (_) => new JSONReply(Info.Network.IsWifi() ? Info.Network.GetWifiStrength().ToString() : "false"));
-			webserver.Get("/api/wifi/ssid", (_) => new JSONReply(Info.Network.IsWifi() ? Info.Network.GetWifiAPSSID() : "false"));
-            webserver.Get("/api/url", (innerurl) =>
-            {
-                if (innerurl.Length > 8)
-                {
-                    lock (cUrl) cUrl = innerurl.Substring(innerurl.IndexOf('?', 8) + 2);
-                }
+		private void SetupServer()
+		{
+			Debug.WriteLine("Starting server");
 
-                return new JSONReply(cUrl);
-            });
-            webserver.Get("/api/refresh", (_) =>
-            {
-                lock (cUrl) cUrl = "refresh";
-                return new JSONReply("refresh - ok");
-            });
+			JSONServerSSL webserverssl = new JSONServerSSL(Info.Network.IP, false, 8081)
+			{
+				RegUrl = new Uri("http://dev.opgs.org/signage/index.php"),
+				ReplyFormat = API4Format
+			};
+			webserverssl.Get("/api", (_) => 
+			{
+				return new JSONReply(APIDescription, "text/html");
+			});
+			webserverssl.Get("/api/os", (_) => 
+			{
+				return new JSONReply(Info.OS.Version);
+			});
+			webserverssl.Get("/api/5", (_) =>
+			{
+				return new JSONReply(String.Format(API4replyformat, Info.OS.Version, Info.Network.IsWifi() ? Info.Network.GetWifiStrength().ToString() : "false", Info.Network.IsWifi() ? Info.Network.GetWifiAPSSID() : "false", cUrl));
+			});
+			webserverssl.Get("/api/url", (url) =>
+			{
+				if (url.Length > 8)
+				{
+					int pos = url.IndexOf('?', 8);
+					cUrl = url.Substring(pos + 2);
+				}
 
-            Task.Run(async () => await webserver.StartAsync().ConfigureAwait(false));
-        }
+				return new JSONReply(cUrl);
+			});
+			webserverssl.Get("/api/refresh", (_) =>
+			{
+				lock (cUrl) cUrl = "refresh";
+				return new JSONReply("refresh - ok");
+			});
+
+			Task.Run(async () => 
+			{
+				await webserverssl.StartAsync();
+			});
+		}
+
+		private void ScreenView_NewWindowRequested(WebView sender, WebViewNewWindowRequestedEventArgs args)
+		{
+			if (args.Uri.Query.Contains("api5"))
+			{
+			}
+			else if (args.Uri.Query.Contains("print"))
+			{
+				sender.Navigate(args.Uri);
+				WebViewBrush wvBrush = new WebViewBrush();
+				wvBrush.SetSource(sender);
+				wvBrush.Redraw();
+				printRect.Fill = wvBrush;
+				print = new PrintJob(printRect);
+				print.Print();
+				sender.Navigate(new Uri(cUrl));
+				args.Handled = true;
+			}
+			else
+			{
+				sender.Navigate(args.Uri);
+				args.Handled = true;
+			}
+		}
 
 		public MainPage()
 		{
@@ -75,15 +122,18 @@ namespace OPG.Signage.Screen
 
 			if (localSettings.Values.ContainsKey("url"))
 			{
-                cUrl = localSettings.Values["url"].ToString();
-            } else {
-                localSettings.Values["url"] = cUrl;
-            }
+				cUrl = localSettings.Values["url"].ToString();
+			} else {
+				localSettings.Values["url"] = cUrl;
+			}
 
 			ScreenView.Navigate(new Uri(cUrl));
+			ScreenView.NewWindowRequested += ScreenView_NewWindowRequested;
 
-            StartRefreshTimer();
-            SetupServer();
+			Debug.WriteLine("Browser done, server next");
+
+			StartRefreshTimer();
+			SetupServer();
 		}
 	}
 }
